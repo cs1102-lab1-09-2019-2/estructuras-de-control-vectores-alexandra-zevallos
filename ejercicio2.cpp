c2348e0a3a561aea5266518f9f44ca55a3654a38
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-28 22:27:13 -0500

    Ejercicio 2 modificado

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,23 +1,52 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
 int moda(vector<int> &lista);
-vector<int> leerConsole();
+vector<int> leerConsola();
 
 vector<int> leerConsola() {
-    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+    int number;
+    vector <int> lista;
+    do {
+      cin>> number;
+      if(number == -1) 
+        break;
+      else
+        lista.push_back(number);
+    } while(true);
+  return lista;
 }
 
 int moda(vector<int> &lista) {
-    /*
-    La moda es el elemento que mas se repite en la lista de elementos
-     */
-    return null;
+    int lista_size, moda_place, moda, repeticiones = 0, mayor = 0;
+    vector <int> repeticiones_totales;
+    lista_size = lista.size();
+    for(int r = 0; r < lista_size; r++) {
+      for(int c = 0; c < (lista_size - 1); c++) 
+        if(r != c) 
+          if(lista[r] == lista[c])
+            repeticiones++;
+      repeticiones_totales.push_back(repeticiones);
+      repeticiones = 0;
+    }
+    for(int a = 0; a < lista_size; a++) 
+      if(repeticiones_totales[a] > mayor)
+        mayor = repeticiones_totales[a];
+    for(int b = 0; b < lista_size; b++)
+      if(mayor == repeticiones_totales[b])
+        moda_place = b;
+    moda = lista[moda_place];
+    return moda;
 }
 
 
 int main() {
     cout<<"Calcular la moda de los numeros ingresados:\n\n";
-    vector<int> leer_usuario = leerConsole();
+    vector<int> leer_usuario = leerConsola();
     int moda_elemento = moda(leer_usuario);
     cout<<"La moda es: "<<moda_elemento<<"\n";
+    return 0;
 }
+
